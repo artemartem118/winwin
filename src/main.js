e7167e13ee2b4d3852fc15d8dd8e7abf0f0899fe
artemartem118 <miraclemarvel118@gmail.com>
2020-08-10 00:20:04 +0300

    add firebase

--- src/main.js
+++ src/main.js
@@ -3,6 +3,20 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import firebase from 'firebase/app'
+import 'firebase/database'
+import 'firebase/storage'
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyCoYXrWg_QdNRoiFtz-pAP0qrhZ6VKKYk0',
+  authDomain: 'winwin-bc12b.firebaseapp.com',
+  databaseURL: 'https://winwin-bc12b.firebaseio.com',
+  projectId: 'winwin-bc12b',
+  storageBucket: 'winwin-bc12b.appspot.com',
+  messagingSenderId: '765837126346',
+  appId: '1:765837126346:web:38c387729e3154c24aa233'
+})
+
 Vue.config.productionTip = false
 
 new Vue({
